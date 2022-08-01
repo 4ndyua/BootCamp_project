53f66a27b4dbb12fd9c9b503e1b4fd1e6e44333e
Andrii Kravchenko <64310996+4ndyua@users.noreply.github.com>
2022-08-01 23:41:55 +0300

    Solution

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
-from TikTokApi import TikTokApi
 from moviepy.editor import VideoFileClip
 import os
+import urllib.request
 
 ROOT_DIR = os.path.abspath(os.curdir)
 
 
-def tiktok2gif(id_: str):
-    with TikTokApi() as api:
-        video = api.video(id=id_)
-
-        video_data = video.bytes()
-
-        with open("out.mp4", "wb") as out_file:
-            out_file.write(video_data)
+def tiktok2gif(url: str):
+    with urllib.request.urlopen(url) as response, \
+            open('out.mp4', 'wb') as file:
+        data = response.read()
+        file.write(data)
 
     vc = VideoFileClip("out.mp4")
     vc.write_gif("gif-out.gif")
+
+    # print(f'{ROOT_DIR}\gif-out.gif')
 
     return f'{ROOT_DIR}\gif-out.gif'
 
 
 s = "Python Bootcamp"
 s_hashed = hash(s)
+
+# print(s_hashed)
